af67c9523b36ed1741954c01a9f008dd1628a6ea
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-10 18:55:00 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	int a[n] = {0};
-	for (int i = 0; i < n; i++) {
+	int a[n];
+	for(int i=0; i<n; ++i)
 		cin >> a[i];
+	int o=0, z=0;
+	for(int i=0; i<n; ++i) {
+		if(a[i]==1)
+			o=i+1;
+		else
+			z=i+1;
 	}
-	int left = 0;
-	int right = 0;
-	for (int i = 0; i < n; i++) {
-		if (a[i] == 0) {
-			left = i + 1;
-		} else {
-			right = i + 1;
-		}
-	}
-	cout << min(left, right) << '\n';
-	return 0;
+	cout << min(o, z) << "\n";
 }
